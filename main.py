ea3c09bf93c8a4fbd68dd4aec6e54d5a54f8443b
samhpr <samuel.hopper23@gmail.com>
2022-12-06 15:47:23 -0800

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#from scipy import stats
+
+df = pd.read_csv("project_midterm_part.csv")
+
+artist_df = df["artistName"]
+ms_df = df["msPlayed"]
+
+
